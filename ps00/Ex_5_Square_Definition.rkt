6c1bd14050f8c04655d0c5521996800dec397be2
Aditya Wate <aditya.wate@hotmail.com>
2014-09-04 09:59:35 -0400

    Problem 5

---
+++ ps00/Ex_5_Square_Definition.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex_5_Square_Definition) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")))))
+(define (sq x)
+  (* x x))
+
+
+;Contract,  purpose statement
+; sq : Number -> Number
+; GIVEN: Number for which square is to be computed
+; RETURNS: Square of the given number.
+
+;Examples
+(sq 3)
+
+(sq 0)
+
+(sq 12)
+
